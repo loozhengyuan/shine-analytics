6a4ffabf5e0a2cdd04d90a091d2350dabeb18a53
LOO ZHENG YUAN <>
2019-03-24 17:10:12 +0800

    allow dynamic creation of flexbox via views.py

--- web/views.py
+++ web/views.py
@@ -9,15 +9,35 @@ def index(request):
 
     # Define plots
     plots = {
-        'User Discount Per Year': test_plot1(),
-        'Annual Compound Annual Growth Rate': test_plot2(),
+        'plot1': top_ten_customer_revenue_contribution_of_all_time(),
+        'plot2': top_ten_customer_revenue_contribution_last_twelve_months(),
+        'plot3': accounts_receivables_balance_by_month(),
+        'plot4': total_income_by_month(),
+        'plot5': total_projects_by_quarter(),
     }
 
-    # Create components and embed to context
+    # Create components
     script, div = components(plots)
+
+    # Create containers and headers
+    divlist = [
+        {
+            "Top 10 Customer Revenue Contribution of All Time": div['plot1'],
+            "Top 10 Customer Revenue Contribution in the Last 12 Months": div['plot2'],
+        },
+        {
+            "Accounts Receivales Balance by Month": div['plot3'],
+            "Total Projects by Quarter": div['plot5'],
+        },
+        {
+            "Total Income by Month": div['plot4'],
+        },
+    ]
+
+    # Append to context
     context = {
         'bokehjs': script,
-        'bokehdiv': div,
+        'bokehdiv': divlist,
     }
 
     return render(request, 'web/bulma_index.html', context=context)
