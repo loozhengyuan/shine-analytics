53f7f5a4ffb3229a8fda600fe5e0f301e9747dbb
LOO ZHENG YUAN <>
2019-04-13 22:31:42 +0800

    add charts for CFO view

--- web/views.py
+++ web/views.py
@@ -17,6 +17,12 @@ def index(request):
             'plot2': total_projects_by_quarter(),
             'plot3': top_ten_customer_revenue_contribution_of_all_time(),
             'plot4': top_ten_customer_revenue_contribution_last_twelve_months(),
+            'plot5': income_by_location(),
+            'plot6': revenue_by_salesperson_per_year(),
+            'plot7': accounts_receivables_balance_by_month(),
+            'plot8': accounts_receivables_turnover_by_month(),
+            'plot9': top_ten_accounts_receivables_balance_by_customer(),
+            'plot10': income_by_project_per_year(),
         }
 
         # Create components
@@ -31,6 +37,18 @@ def index(request):
             {
                 "plot3": div['plot3'],
                 "plot4": div['plot4'],
+            },
+            {
+                "plot5": div['plot5'],
+                "plot6": div['plot6'],
+            },
+            {
+                "plot7": div['plot7'],
+                "plot8": div['plot8'],
+            },
+            {
+                "plot9": div['plot9'],
+                "plot10": div['plot10'],
             },
         ]
 
